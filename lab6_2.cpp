4d5abc3fac4fccbcfbd812036afe4ad99ebae0a7
Your NameCpi2544 <pokpong1789@gmail.com>
2021-11-26 18:15:54 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double a){
+    return a/180*M_PI;
+}
+double rad2deg(double b){
+    return b*180/M_PI;
+}
+double findXComponent(double l1,double l2,double a1,double a2){
+    return l1*cos(a1)+l2*cos(a2);
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+    return l1*sin(a1)+l2*sin(a2); 
+} 
+double pythagoras(double xcomp,double ycomp){
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult(double result_vec_length,double result_vec_direction){
+    cout << "Length of the resultant vector = " << result_vec_length << endl;
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << endl;
+}
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -12,13 +33,19 @@ int main(){
     cin >> l2;
     cout << "Enter direction of the second vector (deg): ";
     cin >> a2;
-    
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+
     a1 = deg2rad(a1);
     a2 = deg2rad(a2);
     xcomp = findXComponent(l1,l2,a1,a2);
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
-    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp));
 
     showResult(result_vec_length,result_vec_direction);
+
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+
+    return 0;
+
 }
