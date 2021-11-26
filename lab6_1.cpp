125bffc56c4fc534f2737b90951ff2604dca2622
Your NameCpi2544 <pokpong1789@gmail.com>
2021-11-26 16:26:32 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
+    int i ,even = 0 ,odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+    while (i != 0)
+    {
+         cout << "Enter an integer: ";
+         cin >> i;
+         if  (i%2 == 0)
+         {
+             even += 1;
+         }else{
+             odd += 1;
+         }
+
+    }
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
