ab81b15802e8e6494646632d908f451ccd2673ba
Your NameCpi2544 <pokpong1789@gmail.com>
2021-11-26 17:06:23 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main(){
     int i ,even = 0 ,odd = 0;
-    cout << "Enter an integer: ";
-    cin >> i;
     while (i != 0)
     {
          cout << "Enter an integer: ";
